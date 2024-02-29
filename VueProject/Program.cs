6b06c1f004cdfcbddf2e6b711fb3dc99ef39c1a5
YPC <op163333@gmail.com>
2024-02-29 11:45:30 +0800

    CRUD非同步修改為同步

--- VueProject/Program.cs
+++ VueProject/Program.cs
@@ -3,42 +3,45 @@ using VueProject.Models;
 
 namespace VueProject
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the DI container.
-            builder.Services.AddControllersWithViews();
+			// Add services to the DI container.
+			builder.Services.AddControllersWithViews();
 
-            builder.Services.AddDbContext<NorthwindContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("Northwind")));
+			builder.Services.AddDbContext<NorthwindContext>(options =>
+				options.UseSqlServer(builder.Configuration.GetConnectionString("Northwind")));
 
 
+			string CorsPolicy = "AllowAny";
+			builder.Services.AddCors(options => {
 
-            var app = builder.Build();
+			});
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
